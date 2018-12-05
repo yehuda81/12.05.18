56da1dd370ce528a68b75e3ee3fefd206016b46b
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-12-05 21:03:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] a = { 2, 3, 5 };
+
+			NewMethod(a);
+		}
+
+		private static double[] NewMethod(int[] array)
+		{
+			double[] myArray = new double[array.Length];
+
+			for(int i = 0; i < array.Length; i++)
+			{
+				myArray[i] = array[i];
+			}
+			return myArray;
+
+
+
+			
+
+		}
+	}
+}
